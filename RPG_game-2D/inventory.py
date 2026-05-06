350d4d9bf7ebb69a90b3ac4e0daedd70cd1fca02
Aymen El yaakoubi <aymaneyaakoubi528@gmail.com>
2026-05-06 13:31:12 +0100

    Delete RPG_game-2D directory

--- RPG_game-2D/inventory.py
+++
@@ -1,9 +0,0 @@
-class Inventory:
-    def __init__(self):
-        self.items = []
-
-    def add_item(self, item):
-        self.items.append(item)
-
-    def show_items(self):
-        print(self.items)
